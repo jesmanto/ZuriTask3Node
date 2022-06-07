19bba4b7d27ca4d7501d1adcd9673e5888c2211c
Jesse Okoro <okorojesse@gmail.com>
2022-06-07 13:36:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log("NUMBER GUESSING GAME\n\n");
+let username = prompt("Please enter your username: ");
+let point = 0;
+let stage = 1;
+let range = 2;
+
+function playGame() {
+  let userOption = prompt("Predict the generated number between 1 and " + range + ": ");
+  const rand = getRandomNumber(range)
+  if (Number(userOption) === rand) {
+    stage++;
+    range++;
+    point++;
+    console.log("\n----\n----\nCorrect. Good job", username,"\n\nYou just earn 1 point", "\n\You are now in stage", stage, "\nWait for the next game >>>\n\n");
+
+    setTimeout(() => { playGame() }, 2000)
+  } else {
+    console.log("\n\nOops!!!\nGame Over\n\n");
+    console.log("*GAME SUMMARY*\n")
+    console.log("Total points: ", getCookie()["points"], "\nStage: ", getCookie()["stage"]);
+  }
+}
+
+const getRandomNumber = (range) => {
+  return Math.floor((Math.random() * range) + 1);
+}
+
+const getCookie = () => {
+  return {
+    username: username,
+    points: point,
+    stage: stage
+  }
+}
+playGame();
